fde7fed1b23d96e6fb77849da28e370e0cd8776b
swswsw <apiswswsw@gmail.com>
2019-09-20 18:23:24 -0700

    get last 100 tx of an address

--- index.js
+++ index.js
@@ -52,3 +52,34 @@ async function fetchMetadata() {
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') { fetchMetadata(); }
 }
+
+const platform = 'tezos';
+const network = 'alphanet';
+const entity = 'operations';
+
+async function listAccountTransactions() {
+    let sendQuery = conseiljs.ConseilQueryBuilder.blankQuery();
+    sendQuery = conseiljs.ConseilQueryBuilder.addFields(sendQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
+    sendQuery = conseiljs.ConseilQueryBuilder.addPredicate(sendQuery, 'kind', conseiljs.ConseilOperator.EQ, ['transaction'], false);
+    sendQuery = conseiljs.ConseilQueryBuilder.addPredicate(sendQuery, 'source', conseiljs.ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
+    sendQuery = conseiljs.ConseilQueryBuilder.addPredicate(sendQuery, 'status', conseiljs.ConseilOperator.EQ, ['applied'], false);
+    sendQuery = conseiljs.ConseilQueryBuilder.addOrdering(sendQuery, 'block_level', conseiljs.ConseilSortDirection.DESC);
+    sendQuery = conseiljs.ConseilQueryBuilder.setLimit(sendQuery, 100);
+
+    let receiveQuery = conseiljs.ConseilQueryBuilder.blankQuery();
+    receiveQuery = conseiljs.ConseilQueryBuilder.addFields(receiveQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
+    receiveQuery = conseiljs.ConseilQueryBuilder.addPredicate(receiveQuery, 'kind', conseiljs.ConseilOperator.EQ, ['transaction'], false);
+    receiveQuery = conseiljs.ConseilQueryBuilder.addPredicate(receiveQuery, 'destination', conseiljs.ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
+    receiveQuery = conseiljs.ConseilQueryBuilder.addPredicate(receiveQuery, 'status', conseiljs.ConseilOperator.EQ, ['applied'], false);
+    receiveQuery = conseiljs.ConseilQueryBuilder.addOrdering(receiveQuery, 'block_level', conseiljs.ConseilSortDirection.DESC);
+    receiveQuery = conseiljs.ConseilQueryBuilder.setLimit(receiveQuery, 100);
+
+    const sendResult = await conseiljs.ConseilDataClient.executeEntityQuery(serverInfo, platform, network, entity, sendQuery);
+    const receiveResult = await conseiljs.ConseilDataClient.executeEntityQuery(serverInfo, platform, network, entity, receiveQuery);
+    const transactions = sendResult.concat(receiveResult).sort((a, b) => { return a['timestamp'] - b['timestamp'] });
+
+    //console.log(`${util.inspect(transactions, false, 2, false)}`);
+    console.log(transactions);
+}
+
+listAccountTransactions();
